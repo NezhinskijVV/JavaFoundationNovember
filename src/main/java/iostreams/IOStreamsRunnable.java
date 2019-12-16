3e15f51f782062955d6bbc6ab68baf078c0e6d9c
NezhinskiyVV <SeekerOfConfessor@gmail.com>
2019-12-17 01:08:11 +0300

    add iostreams

--- src/main/java/iostreams/IOStreamsRunnable.java
+++ src/main/java/iostreams/IOStreamsRunnable.java
@@ -1,34 +1,56 @@
 package iostreams;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class IOStreamsRunnable {
     public static void main(String[] args) {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//        try {
+//            String inputString = bufferedReader.readLine();
+//            System.out.println("Наша строка: " + inputString);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+////        Integer.parseInt(s)
+//        try {
+//            bufferedReader.close();
+//        } catch (IOException e) {
+//            try {
+//                bufferedReader.close();
+//            } catch (IOException ex) {
+//                ex.printStackTrace();
+//            }
+//        }
+//
+//        try {
+//            PrintWriter writer = new PrintWriter(new File("file.txt"));
+//            writer.print("Даниил");
+//            writer.flush();
+//            writer.close();
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+
         try {
-            String inputString = bufferedReader.readLine();
-            System.out.println("Наша строка: " + inputString);
+            System.out.println("Вывод файла");
+            BufferedReader fileReader = new BufferedReader(new FileReader(new
+                    File("C:\\Users\\Vitaliy Nezhinskiy\\IdeaProjects\\JavaFoundationNovember" +
+                    "\\file.txt")));
+            String line;
+            while ((line = fileReader.readLine()) != null) {
+                System.out.println(line);
+            }
+            fileReader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-//        Integer.parseInt(s)
-        try {
-            bufferedReader.close();
-        } catch (IOException e) {
-            try {
-                bufferedReader.close();
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        }
-
 //try with resources
-//        BufferedReader bufferedReader;
-//        try ( InputStreamReader inputStreamReader = new InputStreamReader(System.in))){
-//
-//        }
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
+            bufferedReader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
